18c70aa1bbd154acdfa959b584e4aa62ef986031
Berdin Andrei <byte.in4matic@gmail.com>
2023-10-31 17:23:48 +0500

    lr11

--- AgentPage.xaml.cs
+++ AgentPage.xaml.cs
@@ -23,6 +23,7 @@ namespace Zelentsov_eyes
         public AgentPage()
         {
             InitializeComponent();
+
             var currentAgent = Zelentsov_eyesEntities.GetContext().Agent.ToList();
             AgentsListView.ItemsSource = currentAgent;
         }
@@ -30,15 +31,86 @@ namespace Zelentsov_eyes
         public void updateservices()
         {
             var currentAgent = Zelentsov_eyesEntities.GetContext().Agent.ToList();
-            currentAgent = currentAgent.Where(p => 
-            p.Title.ToLower().Contains(TBSearch.Text.ToLower()) 
-            || p.Phone.Contains(TBSearch.Text.ToLower()) 
+
+
+            //< TextBlock > По возрастанию наименования</ TextBlock > 1 
+            //               < TextBlock > По убыванию наименования</ TextBlock > 2 
+            //                  < TextBlock > По(nw)возрастанию размера скидки </ TextBlock > 3 
+            //                    < TextBlock > По(nw)убыванию размера скидки </ TextBlock > 4 
+            //                      < TextBlock > По возрастанию приоритета агента </ TextBlock > 5
+            //                         < TextBlock > По убыванию приоритета агента </ TextBlock > 6
+
+            //              ЗАО
+            //              МКК
+            //              МФО
+            //              ОАО
+            //              ООО
+            //              ПАО
+
+            //currentAgent = currentAgent.Where(p => p.AgentTypeName.ToString() == CBFilter.Text.ToString()).ToList();
+            switch (CBFilter.SelectedIndex)
+            {
+                case 0:
+                    break;
+                case 1:
+                    currentAgent = currentAgent.Where(p => p.AgentTypeName.ToString() == "ЗАО").ToList();
+                    break;
+                case 2:
+                    currentAgent = currentAgent.Where(p => p.AgentTypeName.ToString() == "МКК").ToList();
+                    break;
+                case 3:
+                    currentAgent = currentAgent.Where(p => p.AgentTypeName.ToString() == "МФО").ToList();
+                    break;
+                case 4:
+                    currentAgent = currentAgent.Where(p => p.AgentTypeName.ToString() == "ОАО").ToList();
+                    break;
+                case 5:
+                    currentAgent = currentAgent.Where(p => p.AgentTypeName.ToString() == "ООО").ToList();
+                    break;
+                case 6:
+                    currentAgent = currentAgent.Where(p => p.AgentTypeName.ToString() == "ПАО").ToList();
+                    break;
+            }
+
+            switch (CBSort.SelectedIndex)
+            {
+                case 0:
+                    break;
+                case 1:
+                    currentAgent = currentAgent.OrderBy(p => p.Title).ToList();
+                    break;
+                case 2:
+                    currentAgent = currentAgent.OrderByDescending(p => p.Title).ToList();
+                    break;
+                case 5:
+                    currentAgent = currentAgent.OrderBy(p => p.Priority).ToList();
+                    break;
+                case 6:
+                    currentAgent = currentAgent.OrderByDescending(p => p.Priority).ToList();
+                    break;
+            }
+
+            
+            currentAgent = currentAgent.Where(p =>
+            p.Title.ToLower().Contains(TBSearch.Text.ToLower())
+            || p.Phone.Replace("+7", "8").Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", "")
+            .Contains(TBSearch.Text.Replace("+7", "8").Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", ""))
             || p.Email.ToLower().Contains(TBSearch.Text.ToLower())).ToList();
 
             AgentsListView.ItemsSource = currentAgent;
         }
 
         private void TBSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateservices();
+        }
+
+        private void CBSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            updateservices();
+        }
+
+        private void CBFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             updateservices();
         }
